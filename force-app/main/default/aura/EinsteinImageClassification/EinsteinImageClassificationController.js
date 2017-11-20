429298f8b34b52c733e95b02b491e1c06f375664
Zayne Turner <16801980+zaynelt@users.noreply.github.com>
2017-11-20 12:15:30 -0800

    Updates to Prediction tab behavior

--- force-app/main/default/aura/EinsteinImageClassification/EinsteinImageClassificationController.js
+++ force-app/main/default/aura/EinsteinImageClassification/EinsteinImageClassificationController.js
@@ -1,27 +1,34 @@
 ({
   readFile: function(component, event, helper) {
-    var files = component.get("v.files");
-    if (files && files.length > 0) {
-      var file = files[0][0];
-      if (file.size>900000) {
-        return alert("The file exceeds the limit of 900kb.");
+      var files = component.get("v.files");
+      if (files && files.length > 0) {
+          //we're getting a falsy value of 1, even w/o file
+          var file = files[0][0];
+          //so we have to implement our actual null check here:
+          if(file){
+              console.log('ACTUALLY IS A FILE.');
+              if (file.size>900000) {
+                  return alert("The file exceeds the limit of 900kb.");
+              }
+              var reader = new FileReader();
+              reader.onloadend = function() {
+                  var dataURL = reader.result;
+                  component.set("v.pictureSrc", dataURL);
+                  component.set("v.fileName", file.name);
+                  helper.upload(component, file.name, dataURL.match(/,(.*)$/)[1]);
+              };
+              reader.readAsDataURL(file);
+          } else {
+              console.log('NOT ACTUALLY IS A FILE.');
+          }          
       }
-      var reader = new FileReader();
-      reader.onloadend = function() {
-        var dataURL = reader.result;
-        component.set("v.pictureSrc", dataURL);
-        component.set("v.fileName", file.name);
-        helper.upload(component, file.name, dataURL.match(/,(.*)$/)[1]);
-      };
-      reader.readAsDataURL(file);
-    }
   },
   switchDefaultUrl: function(component) {
-    component.set(
-      "v.defaultUrl",
-      component.get("v.imageType") === "image"
-        ? "http://einstein.ai/images/mountainvsbeach.zip"
-        : "http://einstein.ai/images/mountainvsbeach.zip"
-    );
+      component.set(
+          "v.defaultUrl",
+          component.get("v.imageType") === "image"
+          ? "http://einstein.ai/images/mountainvsbeach.zip"
+          : "http://einstein.ai/images/mountainvsbeach.zip"
+      );
   }
 });
