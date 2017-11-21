326dadb43841217292e09da1762cd31f74e8108d
Zayne Turner <16801980+zaynelt@users.noreply.github.com>
2017-11-20 17:23:47 -0800

    Layout and Prediction Tab changes

--- force-app/main/default/aura/EinsteinImageClassification/EinsteinImageClassificationController.js
+++ force-app/main/default/aura/EinsteinImageClassification/EinsteinImageClassificationController.js
@@ -1,27 +1,25 @@
 ({
   readFile: function(component, event, helper) {
+   
     var files = component.get("v.files");
-    if (files && files.length > 0) {
-        //we're getting a falsy value of 1, even w/o file
-        var file = files[0][0];
-        //so we have to implement our actual null check here:
-        if(file){
-            console.log('ACTUALLY IS A FILE.');
-            if (file.size>5000000) {
-                return alert("The file exceeds the limit of 5MB.");
-            }
-            var reader = new FileReader();
-            reader.onloadend = function() {
-                var dataURL = reader.result;
-                component.set("v.pictureSrc", dataURL);
-                component.set("v.fileName", file.name);
-                helper.upload(component, file.name, dataURL.match(/,(.*)$/)[1]);
-            };
-            reader.readAsDataURL(file);
-        } else {
-            console.log('NOT ACTUALLY IS A FILE.');
-        }          
-    }
+    //files var is a proxy object w/ length of 1, even w/o file, so we've got to modify our null check
+    var file = files[0][0];      
+    if(file){
+        console.log('ACTUALLY IS A FILE.');
+        if (file.size>5000000) {
+            return alert("The file exceeds the limit of 5MB.");
+        }
+        var reader = new FileReader();
+        reader.onloadend = function() {
+            var dataURL = reader.result;
+            component.set("v.pictureSrc", dataURL);
+            component.set("v.fileName", file.name);
+            helper.upload(component, file.name, dataURL.match(/,(.*)$/)[1]);
+        };
+        reader.readAsDataURL(file);
+    } else {
+        console.log('NOT ACTUALLY IS A FILE.');
+    }                 
   },
   switchDefaultUrl: function(component) {
       component.set(
